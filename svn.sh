f01e2ad8a8acafaf72d24e904028616aa74c5394
Pascal Wacker <wackerpascal@gmail.com>
2017-09-22 12:34:36 +0200

    Update svn.sh to fix the rights
    After svn, the root user will own the svn folder, let's fix that!

--- svn.sh
+++ svn.sh
@@ -1,2 +1,4 @@
 cd /home/ubuntu
 svn co --username <NETZ-USERNAME> --password <NETZ-PASSWORD> --non-interactive https://svn.inf.ethz.ch/svn/systems/SPCA2017/trunk/submissions/<NETZ-USERNAME>
+# fixing owner of svn folder
+sudo chown -R ubuntu:ubuntu /home/ubuntu/
