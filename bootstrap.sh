441a3a472707a66573cd152775ae1491aafb07f2
Pascal Wacker <wackerpascal@gmail.com>
2017-09-25 13:19:44 +0200

    add gcc-multilib and g++-multilib

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,7 +18,7 @@ sudo locale-gen en_GB.UTF-8
 sudo update-locale LANG=en_US.UTF-8 LC_MESSAGES=POSIX
 
 echo "-- Install tools and helpers --"
-sudo apt-get install -y --force-yes build-essential python-software-properties vim nano htop curl git npm subversion nodejs npm
+sudo apt-get install -y --force-yes build-essential python-software-properties vim nano htop curl git npm subversion nodejs npm gcc-multilib g++-multilib
 
 Update
 
